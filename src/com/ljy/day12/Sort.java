86dca061fc27923675cde78f39735c9e2a651594
lijiayan_mail@163.com <lijiayan_mail@163.com>
2018-03-13 13:56:23 +0800

    Hello

---
+++ src/com/ljy/day12/Sort.java
@@ -0,0 +1,51 @@
+package com.ljy.day12;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Sort {
+	public static void main(String[] args) {
+		Random random = new Random();
+		int[] array = new int[10];
+		for (int i = 0; i < 10; i++) {
+			array[i] = random.nextInt(20);
+		}
+		// System.out.println("Ã°ÅİÅÅĞòÇ°");
+		// System.out.println(Arrays.toString(array));
+		// System.out.println("Ã°ÅİÅÅĞòºó");
+		// bubbleSort(array);
+		// System.out.println(Arrays.toString(array));
+		System.out.println("Ñ¡ÔñÅÅĞòÇ°");
+		System.out.println(Arrays.toString(array));
+		System.out.println("Ñ¡ÔñÅÅĞòºó");
+		selectSort(array);
+		System.out.println(Arrays.toString(array));
+	}
+
+	// 1.Ã°ÅİÅÅĞò
+	public static void bubbleSort(int[] array) {
+		for (int i = 0; i < array.length - 1; i++) {
+			for (int j = 0; j < array.length - 1 - i; j++) {
+				if (array[j] > array[j + 1]) {
+					array[j] = array[j] ^ array[j + 1];
+					array[j + 1] = array[j + 1] ^ array[j];
+					array[j] = array[j] ^ array[j + 1];
+				}
+			}
+		}
+
+	}
+
+	// 2.Ñ¡ÔñÅÅĞò
+	public static void selectSort(int[] array) {
+		for (int i = 0; i < array.length - 1; i++) {
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[i] < array[j]) {
+					array[i] = array[i] ^ array[j];
+					array[j] = array[j] ^ array[i];
+					array[i] = array[i] ^ array[j];
+				}
+			}
+		}
+	}
+}
