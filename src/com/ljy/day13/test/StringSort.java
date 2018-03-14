c5fd14a96c2ad3c66916563fb021847c16ff4bcd
lijiayan_mail@163.com <lijiayan_mail@163.com>
2018-03-14 20:12:30 +0800

    update

--- src/com/ljy/day13/test/StringSort.java
+++ src/com/ljy/day13/test/StringSort.java
@@ -9,12 +9,11 @@ public class StringSort {
 		TreeSet<String> set = new TreeSet<>(new Comparator<String>() {
 			@Override
 			public int compare(String o1, String o2) {
-				
 				return o1.compareTo(o2);
 			}
 		});
-		Collections.addAll(set, "12","23","13","45");
+		Collections.addAll(set, "12", "23", "13", "45");
 		System.out.println(set);
 	}
-	
+
 }
